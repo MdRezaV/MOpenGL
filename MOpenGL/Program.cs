1c5dee2b29ed2d0976e8f416717129d1b90b4608
Mmreza Vali <37976092+MdRezaV@users.noreply.github.com>
2023-09-04 10:25:45 +0330

    Add files via upload

---
+++ MOpenGL/Program.cs
@@ -0,0 +1,24 @@
+﻿using MGLCC;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace MOpenGL;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using (var window = new MGLWindow(new GameWindowSettings()
+        {
+            UpdateFrequency = 60,
+        },
+        new NativeWindowSettings()
+        {
+            CurrentMonitor = Monitors.GetMonitors()[1].Handle,
+            WindowState = WindowState.Fullscreen,
+        }))
+        {
+            window.Run();
+        }
+    }
+}
